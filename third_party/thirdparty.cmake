d665770abe13c7c140c8761c85b136a6b4ecb791
ridehuang <ridehuang@tencent.com>
2022-03-30 20:19:41 +0800

    run success on linux

--- third_party/thirdparty.cmake
+++ third_party/thirdparty.cmake
@@ -6,11 +6,11 @@ file(MAKE_DIRECTORY ${EXTERNAL_PREFIX}/lib)
 
 ## clang
 add_library(clang_3rd SHARED IMPORTED GLOBAL)
-if (APPLE)
-    set(CLANG_APPLE_INCLUDE_DIRS "${THIRD_PARTY_DIR}/clang/include")
+if (LINUX)
+    set(CLANG_APPLE_INCLUDE_DIRS "/usr/local/clang-10.0.1/include")
     set_target_properties(clang_3rd PROPERTIES
         IMPORTED_LOCATION
-        ${THIRD_PARTY_DIR}/clang/lib/libclang.dylib
+        "/usr/local/clang-10.0.1/lib/libclang.so.10"
         INTERFACE_INCLUDE_DIRECTORIES
         "${CLANG_APPLE_INCLUDE_DIRS}"
         )
